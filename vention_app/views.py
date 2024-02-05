5f85899e027b35979c17e89926e8550ba5d79f0d
AgnKam <a.kaminska.495@studms.ug.edu.pl>
2024-02-05 23:11:50 +0100

    Now I added DRF and created API endpoints

--- vention_app/views.py
+++ vention_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Task
+from .serializers import CategorySerializer, TaskSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
