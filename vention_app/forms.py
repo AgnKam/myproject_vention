ec6177cc3230092ca656fc92a785669ab9a80277
AgnKam <a.kaminska.495@studms.ug.edu.pl>
2024-02-06 20:06:04 +0100

    Now I fixed date format

--- vention_app/forms.py
+++ vention_app/forms.py
@@ -5,6 +5,9 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'category', 'due_date', 'completed']
+        widgets = {
+            'due_date': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'}),
+        }
 
 class CategoryForm(forms.ModelForm):
     class Meta:
--- vention_app/models.py
+++ vention_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-import datetime
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
@@ -11,4 +12,13 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     due_date = models.DateField(null=True, blank=True) # An extra
+
+    def clean(self):
+        # Custom validation for due_date
+        if self.due_date and self.due_date < timezone.now().date():
+            raise ValidationError("Due date cannot be in the past.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(Task, self).save(*args, **kwargs)
 
