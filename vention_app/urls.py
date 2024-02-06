0a1b86bc5f4c3b2f777b72f1d6179978e944f399
AgnKam <a.kaminska.495@studms.ug.edu.pl>
2024-02-06 15:52:22 +0100

    Now I added some basic of HTML and CSS

--- vention_app/urls.py
+++ vention_app/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, TaskViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),  # Prefixed with 'api/' for your DRF viewsets
+    path('', views.home, name='home'),  # Root URL for your home page
 ]
+
--- vention_app/views.py
+++ vention_app/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .models import Category, Task
 from .serializers import CategorySerializer, TaskSerializer
 from rest_framework import permissions
+from django.http import HttpResponse
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -12,4 +13,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+def home(request):
+    return render(request, 'vention_app/home.html')
 
